59981e28d945fd0851a9f1955f2695e58f19047c
name <gafarova.tasia@gmail.com>
2026-08-25 09:47:16 +0300

    admin add button

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -62,7 +62,15 @@ function AdminPage() {
                                disabled:cursor-not-allowed"
                                required/>
                     </label>
-                    {/*todo: others*/}
+                    <button
+                        type="submit"
+                        className="w-full px-3 py-2 rounded-md border border-gray-300 bg-white text-gray-700
+                        hover:bg-gray-50 hover:border-gray-400
+                        focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent
+                        disabled:opacity-50 disabled:cursor-not-allowed"
+                    >
+                        Save
+                    </button>
                 </form>
             </section>
         </main>
